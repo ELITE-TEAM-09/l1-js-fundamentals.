30192c459895cf92d14216140d8eb81c483f226c
Fardowsa Abdullahi <162545861+fardowsa1234@users.noreply.github.com>
2024-03-19 21:35:04 +0300

    Add files via upload

---
+++ l1-js-fundamentals/index.js
@@ -0,0 +1,110 @@
+// UTILITY
+function printValue(value){
+    console.info(value)
+}
+
+// 1.0 DEFINE VARIABLES
+//TODO: ADD fixed variable
+const name = "Fardowsa";
+
+
+//TODO: ADD redeclarable variable
+var num = 18;
+ 
+//TODO: ADD re-assignable but not redeclarable variable
+let age = 10;
+
+// 2.0 DIFFERENT DATA TYPES
+//TODO: Create a fixed variable with a number value
+const realNum = 21;
+
+//TODO: Create a redeclarable variable with a boolean value
+
+var p = true;
+//TODO: Create a re-assignable variable with a string value
+let lastName = "Abdullahi"
+// 3.0 STRING OPERATIONS
+//TODO: Concatenate string (+)
+const firstName = "Fardowsa";
+const lastname = "Abdullahi";
+console.log(name + " " +lastName )
+console.log(fullName);
+//TODO: Interpolate string (`${}`)
+let greeting = Hello ${firstname} ${lastname}
+console.log(greeting);
+
+console.log(fullname);
+//TODO:Convert to uppercase
+let firstname = Fardowsa
+let uppercasename =Fardowsa.toUppercase ();
+//TODO: Convert to lowercase
+let lastname = Abdullahi
+let lowercase =Fardowsa.toLowercase ();
+
+//TODO: Index a specific character
+let sentence = " Hi Fardowsa"
+let word = sentence.charAt
+console.log(word)
+
+//TODO: PRACTICE ON YOUR OWN (Check mozilla documentation)
+
+
+// 4.0 COMPARISON
+//TODO: strict equality
+const x = 4;
+const y = "4"
+console.log(x===y)
+
+//TODO: strict inequality
+
+console.log(x!==y)
+//TODO: equality
+
+console.log(x==y);
+//TODO: inequality
+console.log(x!=y);
+
+//TODO: greater than
+let num1= 8;
+let num2= 5;
+let greater= num1 > num2;
+//TODO: less than
+let less= num2 < num1;
+
+//TODO: greater or equal to
+let num1= 7;
+let num2= 7;
+let is GreaterOrEqual = num1>=num2;
+
+//TODO: less or equal to
+let num1 = 7;
+let num2 = 7;
+let is LessOrEqual = num1<=num2;
+
+// 5.0 CONTROL FLOW
+// TODO: if-else statement
+let num = 9
+if (num >4){("Number is greater than 4")
+
+} else {('Number is not greater than 4');
+}
+
+//TODO: switch-case
+
+switch (value) {
+case = '3';
+console.log("Got 3");
+break;
+case = '4';
+console.log("Got 4");
+break;
+default:
+    console.log("nothing");
+
+}
+
+
+
+
+
+
